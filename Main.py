2dcc346f5f545d191212d420eecd2cbeaafd9b7d
Leo Westerberg <le8037we-s@student.lu.se>
2021-07-30 23:39:02 +0200

    Progress on run() method

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 from BotManagement import BotManagement
 from DbManagement import DbManagement
-import time
-
-
 class main:
     def main():
         BotManagement(DbManagement()).reset_bot().run_bot()
+        
     if __name__ == "__main__":
         main()
 
